629031eb05c75a60096e1667e77232544a1e8b8c
Gaukhar902 <gaukhar.akhmetova@gmail.com>
2026-07-20 21:20:01 +0500

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.py
@@ -0,0 +1,20 @@
+class MyStack:
+
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        for _ in range(len(self.stack) - 1):
+            self.stack.append(self.stack.popleft())
+
+    def pop(self) -> int:
+        return self.stack.popleft()
+
+    def top(self) -> int:
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        if len(self.stack) > 0:
+            return False
+        else: return True
